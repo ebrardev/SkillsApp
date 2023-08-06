aed06128b8d7ffcbb7c7e3d1eef975e2788a7bb1
ebrardev <16155052@mersin.edu.tr>
2023-08-06 17:48:23 +0300

    add input component

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+import React, { useState } from 'react';
+import SkillsInput from './src/components/SkillsInput';
 export default function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.frame}>
+        <Button style={styles.button} title="Add Skill" color='white'  onPress={openModal} />
+      
+        </View>
+        <SkillsInput visible={openModal} />
+      </View>
+    </>
   );
 }
 
@@ -14,7 +27,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
   },
+  button: {
+  
+    padding: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  frame: {  
+    backgroundColor: 'blue',
+    borderRadius: 15,
+    marginTop: 20,
+  }
+
 });
---
+++ src/components/SkillsInput.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SkillsInput({ visible}) {
+  return (
+    <View>
+      <Text>SkillsInput</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
