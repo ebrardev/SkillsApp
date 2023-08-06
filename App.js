6becf794d1eb483b8ba29bf1b3c1965b47a7ec86
ebrardev <16155052@mersin.edu.tr>
2023-08-06 23:28:46 +0300

    listeleme

--- App.js
+++ App.js
@@ -1,33 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useState } from 'react';
+import React, { useState } from 'react'; // 'react' importunu ekleyin
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import SkillsInput from './components/SkillsInput';
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [skills, setSkills] = useState([]); // skills state'i ekleyin
 
   const startModal = () => {
     setModalIsVisible(true);
   };
+
   const endModal = () => {
     setModalIsVisible(false);
   };
-  const addSkill = (skillTitle) =>{
-    console.log(skillTitle)
-    endModal()
 
-  }
+  const addSkill = (skillTitle) => {
+    setSkills((currentSkills) => [...currentSkills, { id: Math.random().toString(), value: skillTitle }]);
+    endModal();
+  };
+
   return (
     <>
       <StatusBar style="auto" />
       <View style={styles.container}>
-        <Button title=" Add skils" color="blue" 
-        onPress={startModal} 
-        
-        
-        
+        <Button
+          title="Add skills" // " Add skils" -> "Add skills" düzeltmesi
+          color="blue"
+          onPress={startModal}
         />
-        <SkillsInput visible={modalIsVisible} onCancel ={endModal}  onAddSkill={addSkill} />
+        <SkillsInput visible={modalIsVisible} onCancel={endModal} onAddSkill={addSkill} />
+        <View>
+          <FlatList
+            data={skills}
+            keyExtractor={(item) => item.id} // keyExtractor eklenmeli
+            renderItem={(itemData) => (
+              <View style={styles.listItem}>
+                <Text>{itemData.item.value}</Text>
+              </View>
+            )}
+          />
+        </View>
       </View>
     </>
   );
@@ -39,5 +52,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: 50,
     paddingHorizontal: 20,
+  },
+  listItem: { // Stil için listItem düzenlemesi
+    padding: 10,
+    backgroundColor: 'gray',
+    color: 'white',
+    borderRadius: 10,
+    marginVertical: 5,
   },
 });
